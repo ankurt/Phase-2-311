b49a16dfe9e562f726651e35d97c8e5cae5ad3fd
Ankur Toshniwal <ankur@getfbpay.in>
2014-12-01 21:43:43 -0500

    4 tables added to pats_structure.sql

--- pats_structure.sql
+++ pats_structure.sql
@@ -2,4 +2,50 @@
 --
 -- by (student_1) & (student_2)
 --
---
+CREATE TABLE owners (
+  id SERIAL,
+  first_name VARCHAR(100),
+  last_name VARCHAR(100),
+  street VARCHAR(100),
+  city VARCHAR(100),
+  state VARCHAR(2),
+  zip VARCHAR(15),
+  phone VARCHAR(20),
+  email VARCHAR(100),
+  active BOOLEAN
+);
+
+CREATE TABLE pets (
+  id SERIAL,
+  name VARCHAR(100),
+  -- animal_id INT,
+  -- owner_id INT,
+  female BOOLEAN,
+  date_of_birth DATE,
+  active BOOLEAN
+);
+
+CREATE TABLE visits (
+  id SERIAL,
+  -- pet_id INT,
+  date DATE,
+  weight INT,
+  overnight_stay BOOLEAN,
+  total_charge INT
+);
+
+CREATE TABLE animals (
+  id SERIAL,
+  name VARCHAR(100),
+  active BOOLEAN
+  );
+
+CREATE TABLE medicines (
+  id SERIAL,
+  name VARCHAR(100),
+  description TEXT,
+  stock_amount INT,
+  method VARCHAR(100),
+  unit VARCHAR(100),
+  vaccine BOOLEAN
+);
