f86d98d687dc7e3f0efc427dc24ad62e2849236f
Louie Kevin <klouie@andrew.cmu.edu>
2014-12-06 17:57:43 -0500

    added overnight stay function

--- pats_functions.sql
+++ pats_functions.sql
@@ -1,6 +1,6 @@
 -- FUNCTIONS AND TRIGGERS FOR PATS DATABASE
 --
--- by (student_1) & (student_2)
+-- by (klouie) & (ankurt)
 --
 --
 -- calculate_total_costs
@@ -11,6 +11,27 @@
 
 -- calculate_overnight_stay
 -- (associated with a trigger: update_overnight_stay_flag)
+CREATE OR REPLACE function calculate_overnight_stay(id SERIAL) RETURNS TRIGGER AS $$
+	DECLARE
+		procedure_time INT;
+	BEGIN 
+		procedure_time = (SELECT SUM(procedures.length_of_time) FROM procedures 
+			JOIN treatments ON procedures.id = treatments.procedure_id 
+			JOIN visits ON treatments.visit_id = visits.id
+			WHERE visits.id = id);
+		IF procedure_time > 720 THEN
+			UPDATE visits SET overnight_stay = true;
+		ELSE
+			UPDATE visits SET overnight_stay = false;
+		END IF;
+		RETURN NULL;
+	END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_overnight_stay_flag
+AFTER UPDATE ON visits
+EXECUTE PROCEDURE calculate_overnight_stay(visits.id);
+
 
 
 
