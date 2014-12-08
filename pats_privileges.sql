d1b82ddd26c36c59081b12c59d6d1b2e4efbd177
Louie Kevin <klouie@andrew.cmu.edu>
2014-12-08 01:13:24 -0500

    added privileges

--- pats_privileges.sql
+++ pats_privileges.sql
@@ -5,6 +5,15 @@
 --
 -- SQL needed to create the pats user
 CREATE USER pats;
+ALTER USER pats NOSUPERUSER;
 
 -- SQL to limit pats user access on key tables
-
+GRANT ALL PRIVILEGES ON owners TO pats;
+GRANT ALL PRIVILEGES ON pets TO pats;
+GRANT ALL PRIVILEGES ON visits TO pats;
+GRANT ALL PRIVILEGES ON animals TO pats;
+GRANT ALL PRIVILEGES ON medicines TO pats;
+GRANT ALL PRIVILEGES ON procedures TO pats;
+GRANT ALL PRIVILEGES ON treatments TO pats;
+GRANT ALL PRIVILEGES ON notes TO pats;
+GRANT ALL PRIVILEGES ON users TO pats;
